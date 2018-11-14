5bfae9c5f7f22fcfde11924f6109ffb01016df9c
Kevin <kevin.s.kim@berkeley.edu>
2018-11-14 11:38:36 -0800

    somewhat doing something

--- app/src/main/java/com/example/calnourish/SearchActivity.java
+++ app/src/main/java/com/example/calnourish/SearchActivity.java
@@ -1,19 +1,23 @@
 package com.example.calnourish;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class SearchActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_search);
+
+        TextView title = (TextView) findViewById(R.id.text1);
+        title.setText("This is Search");
 
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavView_Bar);
         Menu menu = bottomNavigationView.getMenu();
@@ -25,23 +29,27 @@ public class MainActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()){
                     case R.id.info:
-                        Intent infoIntent = new Intent(MainActivity.this, InfoActivity.class);
+                        Intent infoIntent = new Intent(SearchActivity.this, InfoActivity.class);
                         startActivity(infoIntent);
                         break;
+
                     case R.id.category:
-                        Intent categoryIntent = new Intent(MainActivity.this, InfoActivity.class);
+                        Intent categoryIntent = new Intent(SearchActivity.this, MainActivity.class);
                         startActivity(categoryIntent);
                         break;
+
                     case R.id.search:
-                        Intent searchIntent = new Intent(MainActivity.this, SearchActivity.class);
+                        Intent searchIntent = new Intent(SearchActivity.this, SearchActivity.class);
                         startActivity(searchIntent);
                         break;
+
                     case R.id.foodrecovery:
-                        Intent foodrecoveryIntent = new Intent(MainActivity.this, InfoActivity.class);
+                        Intent foodrecoveryIntent = new Intent(SearchActivity.this, MainActivity.class);
                         startActivity(foodrecoveryIntent);
                         break;
+
                     case R.id.menu:
-                        Intent menuIntent = new Intent(MainActivity.this, InfoActivity.class);
+                        Intent menuIntent = new Intent(SearchActivity.this, MainActivity.class);
                         startActivity(menuIntent);
                         break;
                 }
